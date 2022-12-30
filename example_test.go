138fb988ed265a6134bbee6499c7860e9e548fdf
Darren Jacques <dpjacques@google.com>
2022-12-29 20:52:24 -0800

    Fix clockwork tests.
    - Only run TestFakeTicker_DeliveryOrder once per invocation.
    - Use channels to avoid race condition in TestFakeClockTimer_ResetRace.
    - Run tests in parallel.
    
    Tested:
    - Run 10,000 times with 2 timeout flakes.
    - Ran with `-race`, no race conditions reported.

--- example_test.go
+++ example_test.go
@@ -6,21 +6,20 @@ import (
 	"time"
 )
 
-// myFunc is an example of a time-dependent function, using an
-// injected clock
+// myFunc is an example of a time-dependent function, using an injected clock.
 func myFunc(clock Clock, i *int) {
 	clock.Sleep(3 * time.Second)
 	*i += 1
 }
 
-// assertState is an example of a state assertion in a test
+// assertState is an example of a state assertion in a test.
 func assertState(t *testing.T, i, j int) {
 	if i != j {
 		t.Fatalf("i %d, j %d", i, j)
 	}
 }
 
-// TestMyFunc tests myFunc's behaviour with a FakeClock
+// TestMyFunc tests myFunc's behaviour with a FakeClock.
 func TestMyFunc(t *testing.T) {
 	var i int
 	c := NewFakeClock()
@@ -32,18 +31,18 @@ func TestMyFunc(t *testing.T) {
 		wg.Done()
 	}()
 
-	// Wait until myFunc is actually sleeping on the clock
+	// Wait until myFunc is actually sleeping on the clock.
 	c.BlockUntil(1)
 
-	// Assert the initial state
+	// Assert the initial state.
 	assertState(t, i, 0)
 
-	// Now advance the clock forward in time
+	// Now advance the clock forward in time.
 	c.Advance(1 * time.Hour)
 
-	// Wait until the function completes
+	// Wait until the function completes.
 	wg.Wait()
 
-	// Assert the final state
+	// Assert the final state.
 	assertState(t, i, 1)
 }
