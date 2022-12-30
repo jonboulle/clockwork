e9b4e2cce7b34d2597c6bed3fdcc47377f91b9e4
Darren Jacques <dpjacques@google.com>
2022-12-30 10:12:51 -0800

    Move logic to fakeClock to reduce Ticker and Timer code complexity.
    - Minimize the custom logic of Tickers and Timers.
    - Don't allow Tickers and Timers access to the lock of the fakeClock
      that controls them. Use closures instead.
    - Use a common `expirer` interface for Timers and Tickers, rather than
      resuing Timers for both.
    - Create a `firer` struct to handle commonalities between Tickers and
      Timers.
    - Use a single function & code path for Timers, regardless of if they
      are made with NewTimer or AfterFunc.
    - Use the mutex hat pattern and add documentation on what the mutex
      protects.
    - Change field names from "sleeper" to "waiter" and add documentation.
    - Various documentation updates.

--- ticker.go
+++ ticker.go
@@ -1,29 +1,48 @@
 package clockwork
 
-import (
-	"time"
-)
+import "time"
 
-// Ticker provides an interface which can be used instead of directly
-// using the ticker within the time module. The real-time ticker t
-// provides ticks through t.C which becomes now t.Chan() to make
-// this channel requirement definable in this interface.
+// Ticker provides an interface which can be used instead of directly using
+// [time.Ticker]. The real-time ticker t provides ticks through t.C which
+// becomes t.Chan() to make this channel requirement definable in this
+// interface.
 type Ticker interface {
 	Chan() <-chan time.Time
+	Reset(d time.Duration)
 	Stop()
 }
 
 type realTicker struct{ *time.Ticker }
 
-func (rt realTicker) Chan() <-chan time.Time {
-	return rt.C
+func (r realTicker) Chan() <-chan time.Time {
+	return r.C
 }
 
 type fakeTicker struct {
-	fakeTimer
+	firer
+
+	// reset and stop provide the implmenetation of the respective exported
+	// functions.
+	reset func(d time.Duration)
+	stop  func()
+
+	// The duration of the ticker.
+	d time.Duration
 }
 
-func (ft *fakeTicker) Stop() {
-	// Ignore returned bool to make signature match.
-	ft.fakeTimer.Stop()
+func (f *fakeTicker) Reset(d time.Duration) {
+	f.reset(d)
+}
+
+func (f *fakeTicker) Stop() {
+	f.stop()
+}
+
+func (f *fakeTicker) expire(now time.Time) *time.Duration {
+	// Never block on expiration.
+	select {
+	case f.c <- now:
+	default:
+	}
+	return &f.d
 }
